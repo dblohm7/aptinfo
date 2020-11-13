5e6d89fa7bcf9eb95379d3d18454054fe5f1c586
Aaron Klotz <aklotz@mozilla.com>
2020-11-13 15:49:01 -0700

    Supply clsid on command line

--- src/main.cpp
+++ src/main.cpp
@@ -4,35 +4,109 @@
 
 #include <windows.h>
 #include <objbase.h>
-#include <locationapi.h>
-#include <mmdeviceapi.h>
 
 #pragma comment(lib, "advapi32.lib")
 #pragma comment(lib, "ole32.lib")
-#pragma comment(lib, "locationapi.lib")
-#pragma comment(lib, "mmdevapi.lib")
 
-#define GET_CLSID_STR_3(x) #x
-#define GET_CLSID_STR_2(x) GET_CLSID_STR_3(x)
-#define GET_CLSID_STR GET_CLSID_STR_2(CHECK_CLSID)
 
-#define GET_CLSID_2(x) x
-#define GET_CLSID GET_CLSID_2(CHECK_CLSID)
+template <typename T, size_t N>
+static inline constexpr size_t ArrayLength(T (&aArr)[N])
+{
+  return N;
+}
 
-static const int kGuidLenWithBracesNul = 39;
-static const size_t kThreadingModelBufChars = 10; // ArrayLength(L"Apartment")
+static constexpr int kGuidLenWithBracesInclNul = 39;
+static constexpr int kGuidLenWithBracesExclNul = kGuidLenWithBracesInclNul - 1;
+static constexpr size_t kThreadingModelBufChars = ArrayLength(L"Apartment");
 
-int wmain(int argc, wchar_t* argv[]) {
-  wchar_t guidBuf[kGuidLenWithBracesNul];
-  if (::StringFromGUID2(GET_CLSID, guidBuf, kGuidLenWithBracesNul) != kGuidLenWithBracesNul) {
-    wprintf(L"Failed to convert \"%S\" to a string\n", GET_CLSID_STR);
+static wchar_t gStrClsid[kGuidLenWithBracesInclNul];
+static wchar_t gStrIid[kGuidLenWithBracesInclNul];
+static CLSID gClsid;
+static IID gIid;
+static bool gHasIid = false;
+
+static void
+Usage(const wchar_t* aArgv0, const wchar_t* aMsg = nullptr)
+{
+  if (aMsg) {
+    fwprintf_s(stderr, L"Error: %s\n\n", aMsg);
+  }
+
+  fwprintf_s(stderr, L"Usage: %s <ProgID or CLSID> [IID]\n\n", aArgv0);
+  fwprintf_s(stderr, L"    CLSID and IID must be specified in registry format,\n    including dashes and curly braces\n");
+}
+
+static bool
+ParseArgv(const int argc, wchar_t* argv[])
+{
+  if (argc < 2) {
+    Usage(argv[0]);
+    return false;
+  }
+
+  // First, get the CLSID.
+  if (argv[1][0] == L'{' && wcslen(argv[1]) == kGuidLenWithBracesExclNul) {
+    // Assume it's a guid.
+    if (wcscpy_s(gStrClsid, argv[1])) {
+      return false;
+    }
+
+    if (FAILED(::CLSIDFromString(gStrClsid, &gClsid))) {
+      Usage(argv[0], L"Failed to parse CLSID");
+      return false;
+    }
+
+  } else {
+    // Assume it's a ProgID
+    if (FAILED(::CLSIDFromProgID(argv[1], &gClsid))) {
+      Usage(argv[0], L"Invalid ProgID");
+      return false;
+    }
+
+    wprintf_s(L"Found ProgID \"%s\"\n", argv[1]);
+
+    if (!::StringFromGUID2(gClsid, gStrClsid, ArrayLength(gStrClsid))) {
+      return false;
+    }
+  }
+
+  wprintf_s(L"Using CLSID %s\n", gStrClsid);
+
+  if (argc == 2) {
+    // No optional IID provided, continue
+    return true;
+  }
+
+  // IID sanity checks
+  if (wcslen(argv[2]) != kGuidLenWithBracesExclNul || argv[2][0] != L'{') {
+    Usage(argv[0], L"Invalid IID");
+    return false;
+  }
+
+  // Assume it's a guid.
+  if (wcscpy_s(gStrIid, argv[2])) {
+    return false;
+  }
+
+  if (FAILED(::IIDFromString(gStrIid, &gIid))) {
+    Usage(argv[0], L"Failed to parse IID");
+    return false;
+  }
+
+  wprintf_s(L"Using IID %s\n", gStrIid);
+
+  gHasIid = true;
+  return true;
+}
+
+int
+wmain(int argc, wchar_t* argv[]) {
+  if (!ParseArgv(argc, argv)) {
     return 1;
   }
 
-  wprintf(L"CHECK_CLSID is %S: %s\n", GET_CLSID_STR, guidBuf);
-
   std::wstring subKey(L"CLSID\\");
-  subKey += guidBuf;
+  subKey += gStrClsid;
   subKey += L"\\InprocServer32";
 
   wchar_t threadingModelBuf[kThreadingModelBufChars] = {};
